40917393e3ff81510507e15813ba9cd942148f3d
darkiox <mrdarkiox@gmail.com>
2022-04-20 15:55:52 -0400

    Terminando hitos, falta fuerza bruta, no funciona

--- hito3CL.py
+++ hito3CL.py
@@ -7,54 +7,73 @@ from random import seed
 from random import randint
 seed(27381)
 driver = webdriver.Chrome()
-passposible = ['contraContra1!','contraContra2!','contraContra3!']
+passposible = ['contraContra1!','contraContra2!','contraContra3!','contraNueva1!']
 passelegida = passposible[2]
 
 #Obtención de un correo temporal
-#driver.get("https://correotemporal.org/")
-driver.get("https://www.google.com") #En pruebas
-correo = driver.current_window_handle
-#----------------
-correotest = "jada50v_z156s@hxsni.com"
-clavetest = "ContraSuperSegura"
-#----------------
+driver.get("https://correotemporal.org/")
+correo = driver.current_window_handle #Guardar ventana de correo
+
 #Abrir Trulustore en nueva pestaña
 driver.execute_script("window.open('https://trulustore.cl/mi-cuenta/','trulu');")
 time.sleep(5)
+
 #Obtener el correo temporal generado en correotemporal.org
-#temp_email = driver.find_element(By.ID, "emailtemporal").get_attribute("value")
+temp_email = driver.find_element(By.ID, "emailtemporal").get_attribute("value")
 
 #Cambiar a pestaña trulustore
 driver.switch_to.window("trulu")
 
 #Ingresar correo y contraseña
-#Por ahora se testea con un login
-inputcorreo = driver.find_element(By.ID,"username")#driver.find_element(By.ID, "reg_email")
-inputcorreo.send_keys(correotest)
+inputcorreo = driver.find_element(By.ID, "reg_email")
+inputcorreo.send_keys(temp_email)
 #print("Correo utilizado: "+temp_email)
-inputpass = driver.find_element(By.ID, "password")#driver.find_element(By.ID, "reg_password")
+inputpass = driver.find_element(By.ID, "reg_password")
 print("Contraseña utilizada: " + passelegida)
-inputpass.send_keys(clavetest)
+inputpass.send_keys(passelegida)
 inputpass.send_keys(Keys.ENTER)
 time.sleep(5)
+
+#Sección de cambio de contraseña con login
+driver.find_element(By.PARTIAL_LINK_TEXT,"Detalles de la cue").click()
+time.sleep(1)
+inpname = driver.find_element(By.ID, "account_first_name")
+inpname.send_keys("Tulio")
+inplname = driver.find_element(By.ID, "account_last_name")
+inplname.send_keys("Trivinho")
+inppasscurrent = driver.find_element(By.ID, "password_current")
+inppasscurrent.send_keys(passelegida)
+inppassnew1 = driver.find_element(By.ID, "password_1")
+inppassnew1.send_keys(passposible[3])
+inppassnew2 = driver.find_element(By.ID, "password_2")
+inppassnew2.send_keys(passposible[3])
+inppassnew2.send_keys(Keys.ENTER)
+time.sleep(10)
 
 #Cerrar sesión
 driver.find_element_by_partial_link_text("Cerrar sesi").click()
 
+#Sección de reestablecimiento de contraseña con correo
+driver.find_element(By.PARTIAL_LINK_TEXT,"Olvidaste la contra").click()
+olvidaste = driver.find_element(By.ID,"user_login")
+olvidaste.send_keys(temp_email)
+olvidaste.send_keys(Keys.ENTER)
+time.sleep(10)
+driver.switch_to.window(correo)
+time.sleep(10)
+driver.switch_to.window("trulu")
+
+
+
 #Fuerza bruta para inicio de sesión
-inputcorreo = driver.find_element(By.ID,"username")
-inputcorreo.send_keys(correotest)
-inputbrute = driver.find_element(By.ID,"password")
+
 for x in passposible:
+    inputcorreo = driver.find_element(By.ID,"username")
+    inputcorreo.send_keys(temp_email)
+    inputbrute = driver.find_element(By.ID,"password")
     inputbrute.send_keys(x)
-    #inputbrute.send_keys(Keys.ENTER)
+    inputbrute.send_keys(Keys.ENTER)
     time.sleep(30)
 
-#Sección de reestablecimiento de contraseña
-
-#driver.find_element(By.PARTIAL_LINK_TEXT,"Olvidaste la contra").click()
-#olvidaste = driver.find_element(By.ID,"user_login")
-#olvidaste.send_keys(correotest)
-#olvidaste.send_keys(Keys.ENTER)
 
 #driver.quit()
--- hito3EU.py
+++ hito3EU.py
@@ -5,36 +5,74 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
 import time, random, string
 driver = webdriver.Chrome()
+passposible = ['contraContra1!','contraContra2!','contraContra3!','contraNueva1!']
+passelegida = passposible[2]
+
+#Obtención de un correo temporal
+driver.get("https://correotemporal.org/")
+correo = driver.current_window_handle #Guardar ventana de correo
+
+#Abrir Aussar en nueva pestaña
+driver.execute_script("window.open('https://www.aussar.es/iniciar-sesion?create_account=1','aussar');")
+time.sleep(5)
+
+#Obtener el correo temporal generado en correotemporal.org
+temp_email = driver.find_element(By.ID, "emailtemporal").get_attribute("value")
+
+#Cambiar a pestaña Aussar
+driver.switch_to.window("aussar")
+
+#Registro
+driver.get("https://www.aussar.es/iniciar-sesion?create_account=1")
+inputgender = driver.find_element(By.ID, "field-id_gender-1") #Click
+inputname = driver.find_element(By.ID, "field-firstname")
+inputlname = driver.find_element(By.ID, "field-lastname")
+inputmail = driver.find_element(By.ID, "field-email")
+inputpass = driver.find_element(By.ID, "field-password")
+inputToS = driver.find_element(By.ID, "acceptLopd") #Click
+inputgender.click()
+inputname.send_keys("Tulio")
+inputlname.send_keys("Trivinho")
+inputmail.send_keys(temp_email)
+inputpass.send_keys(passelegida)
+inputToS.click()
+time.sleep(2)
+driver.find_element(By.XPATH, '//*[@id="customer-form"]/footer/button').click()
+time.sleep(10)
 
 
-menu = 1
-while(menu!=0):
-    print("Ingrese su opcion: \n")
-    print(" 1 - Registro automático \n")
-    print(" 2 - Recuperar contraseña \n")
-    print(" 3 - Bruteforce \n")
-    print(" 4 - Cambiar contraseña con login \n")
-    print(" 0 - Salir \n")
-    if(menu == 1): #Registro
-        driver.get("https://www.aussar.es/iniciar-sesion?create_account=1")
-        inputgender = driver.find_element(By.ID, "field-id_gender-1") #Click
-        inputname = driver.find_element(By.ID, "field-firstname")
-        inputlname = driver.find_element(By.ID, "field-lastname")
-        inputmail = driver.find_element(By.ID, "field-email")
-        inputpass = driver.find_element(By.ID, "field-password")
-        inputToS = driver.find_element(By.ID, "acceptLopd") #Click
+#Cambiar contraseña con login
+driver.find_element(By.XPATH,'//*[@id="header"]/nav/div[1]/div/div/div/div/div[2]/div/ul/li[1]/a').click()
+time.sleep(2)
+driver.find_element(By.XPATH, '//*[@id="identity-link"]/span').click()
+inpoldpass = driver.find_element(By.ID, "field-password")
+inpoldpass.send_keys(passelegida)
+inpnewpass = driver.find_element(By.ID, "field-new_password")
+inpnewpass.send_keys(passposible[3])
+time.sleep(2)
+inputToS2 = driver.find_element(By.ID, "acceptLopd") #Click
+inputToS2.click()
+time.sleep(2)
+submit = driver.find_element(By.XPATH, '//*[@id="customer-form"]/footer/button')
+time.sleep(10)
+driver.find_element(By.XPATH, '//*[@id="wrapper"]/div/nav/ol/li[2]/a').click()
+time.sleep(2)
+driver.find_element(By.XPATH, '//*[@id="main"]/footer/div/a').click()
 
-        inputgender.click()
-        inputname.send_keys("Tulio")
-        inputlname.send_keys("Trivinho")
-        inputmail.send_keys("tulitotrivinho@gmail.com")
-        inputpass.send_keys("juaninteamo")
-        inputToS.click()
-        #driver.find_element(By.ID, "submit-login").click()
-    elif(menu == 2): #Recuperar contraseña
-        driver.get("https://www.aussar.es/recuperar-contrase%C3%B1a")
-        inputmailr = driver.find_element(By.ID, "email")
-        inputmailr.send_keys("tulitotrivinho@gmail.com")
-        #inputmailr.send_keys(Keys.ENTER)
-    elif(menu == 3): #Fuerza bruta
-        driver.get("https://www.aussar.es/iniciar-sesion")
+#Recuperar contraseña
+driver.find_element(By.XPATH, '//*[@id="login-form"]/section/div[3]/a').click()
+inputmailr = driver.find_element(By.ID, "email")
+inputmailr.send_keys(temp_email)
+inputmailr.send_keys(Keys.ENTER)
+time.sleep(10)
+#Fuerza bruta
+driver.find_element(By.XPATH, '//*[@id="back-to-login"]').click()
+for x in passposible:
+    loginmail = driver.find_element(By.ID, "field-email")
+    loginmail.send_keys(temp_email)
+    loginpass = driver.find_element(By.ID, "field-password")
+    loginpass.send_keys(passposible[0])
+    loginpass.send_keys(Keys.ENTER)
+    if "mi-cuenta" in driver.current_url:
+        break
+    time.sleep(5)
